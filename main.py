8dfd06e76eab14e5a1d84b5374b73eb7fbf381e7
Ishan Venkatesh <ishanv.official@gmail.com>
2026-07-16 12:02:26 -0700

    Add function to display menu

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 def main():
-    pass
+    expenses = []
 
+def display_menu():
+    print("==== Personal Finance Tracker ====\n")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Search Expenses")
+    print("4. Total Spending")
+    print("5. Quit")
+
+
+display_menu()
